581975f0c7a5075f8125b0e5b5fccaa08e59c26a
Christian McKain <cmckain@pop-os.localdomain>
2019-09-17 14:26:58 -0400

    new vuex store for sharing values

---
+++ eaui/src/store/index.js
@@ -0,0 +1,13 @@
+// Copyright 2019, Endless Abyss Team
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+//new pages are added here in the form
+//import 'page' from 'filepath'
+import * as types from './mutation-types'
+
+Vue.use(Vuex)
+
+const initialState = { 
+    
+}
