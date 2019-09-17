30a164cc0d131fcc33ff2aa8e474466946b3b105
Christian McKain <cmckain@pop-os.localdomain>
2019-09-17 16:09:20 -0400

    Public view for the webpage

---
+++ eaui/src/views/PublicApp.vue
@@ -0,0 +1,15 @@
+<!-- The Endless Abyss -->
+<template>
+    <div id="publicApp">
+        <router-view />
+    </div>
+</template>
+
+<script>
+import Login from '@/views/Login'
+export default {
+    name: 'PublicApp',
+    components: { Login },
+    created() {}
+}
+</script>
