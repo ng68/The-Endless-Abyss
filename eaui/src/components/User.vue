8ed8979e1d537f94237554c23b776fed521ab37d
Christian McKain <cmckain@pop-os.localdomain>
2019-09-20 17:32:27 -0400

    we have a login page, no linking done yet

---
+++ eaui/src/components/User.vue
@@ -0,0 +1,96 @@
+<!-- Copyright 2019, Edge Case Research, Inc. -->
+<!-- Confidential -->
+<template>
+  <div id="login">
+    <div class="box">
+              <div class="welcomeHeader">
+                <h1>Login</h1>
+                <small>You risk entering the Abyss!</small>
+              </div>
+              <form>
+                <label for="">Username</label>
+                <div class="field">
+                  <div class="control">
+                    <input
+                      v-model="username"
+                      v-model.trim="login.username"
+                      type="text"
+                      class="username input"
+                      qa-login="username"
+                      autofocus
+                    />
+                  </div>
+                </div>
+                <label for="">Password</label>
+                <div class="field">
+                  <div class="control">
+                    <input
+                      v-model="password"
+                      v-model.trim="login.password"
+                      type="password"
+                      class="password input"
+                      qa-login="password"
+                    />
+                  </div>
+                </div>
+                <button
+                  class="button is-block is-info is-fullwidth mt50"
+                  qa-login="login"
+                  @click="onSubmit"
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+  </div>
+</template>
+
+<script>
+
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'Login',
+  components: {
+
+  },
+
+  data: () => ({
+    username: '',
+    password: '',
+    login: {}
+  }),
+
+  computed: {},
+
+  methods: Object.assign(
+    {
+      async onSubmit(evt) {
+        evt.preventDefault()
+        if (!this.username || !this.password) {
+          this.$toast.open({
+            duration: 5000,
+            message: 'Please add a username and password',
+            position: 'is-bottom',
+            type: 'is-danger'
+          })
+        } else {
+          try {
+            await this.authenticateUser(this.login)
+            this.$router.push({ name: 'Home', query: { tab: 'sut', page: 1 } })
+          } catch (e) {
+            this.$toast.open({
+              duration: 5000,
+              message: 'Incorrect login. Please try again',
+              position: 'is-bottom',
+              type: 'is-danger'
+            })
+          }
+        }
+      }
+    },
+    mapActions(['authenticateUser'])
+  )
+}
+</script>
+
--- eaui/src/main.js
+++ eaui/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import User from './components/User'
 import HelloWorld from './components/HelloWorld'
 
 Vue.config.productionTip = false
@@ -9,6 +10,7 @@ Vue.use(VueRouter)
 
 const routes = [
   { path: '/helloworld', component: HelloWorld },
+  { path: '/login', component: User},
   { path: '/', components: HelloWorld}
 ]
 
