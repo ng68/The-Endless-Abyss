8731e968ff8c58f9084a320568659cdd6683c9a8
Christian McKain <cmckain@pop-os.localdomain>
2019-09-20 17:21:34 -0400

    Not login but routing works

--- eaui/src/main.js
+++ eaui/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/helloworld', component: HelloWorld },
+  { path: '/', components: HelloWorld}
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
