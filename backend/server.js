09da70f452cfa4c4f956b3de35f6a1b899782b5e
Edhballa7 <compton0@purdue.edu>
2019-09-24 14:24:06 -0400

    First deploy of server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,31 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
+var mysql = require('mysql');
+
+app.use(bodyParser.json());
+
+var con = mysql.createConnection({
+  host: 'remotemysql.com',
+  user: 'oBIw78M3tx',
+  password: 'IgLMVTpxnE',
+  port: '3306'
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database!");
+});
+
+app.post("/url", (req, res, next) => {
+    var obj = req.body;
+    console.log(obj.Hello);
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
 
 app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
+    res.json(["Hello", "World"]);
+});
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
