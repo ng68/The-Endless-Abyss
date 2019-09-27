b74c92545905107d42e302ae7b43605bd038466c
Edhballa7 <compton0@purdue.edu>
2019-09-26 22:16:22 -0400

    Fixed newuser

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,10 @@ app.post("/newuser", (req, res, next) => {
     var password = obj.user.password;
     var email = obj.user.email;
     var sql;
+
+    //Encrypt email and password
     
-    //Check to see if email alrady exists
+    //Check to see if email already exists
     sql = "SELECT * FROM User WHERE email = '" + email + "'";
     con.query(sql, function(err, result) {
       if (err) throw err;
@@ -40,7 +42,7 @@ app.post("/newuser", (req, res, next) => {
           if (result2.length > 0) {
             res.json("Failure Username");
           }else {
-            sql = "INSERT INTO User (email, username, password) VALUES ('" + username + "', '" + password  + "', '" + email + "')";
+            sql = "INSERT INTO User (email, username, password) VALUES ('" + email + "', '" + username  + "', '" + password + "')";
             con.query(sql, function(err, result3) {
                 if (err) throw err;
                 console.log("1 record inserted!");
@@ -57,8 +59,19 @@ app.post("/login", (req, res, next) => {
   var obj = req.body;
   var username = obj.user.username;
   var password = obj.user.password;
+  var sql;
+  //Decrypt password
+
   //Do check for email/username and password
-  res.json([username, password]);
+  sql = "SELECT * FROM User WHERE (username = '" + username + "') AND (password = '" + password + "')";
+  con.query(sql, function(err, result) {
+    if (err) throw err;
+    if (result.length > 0) {
+      res.json("Success");
+    }else {
+      res.json("Failure");
+    }
+  });
 });
 
 //Recover password
