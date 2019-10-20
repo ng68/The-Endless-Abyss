79f4b492535afa9b61c1ed254533303637db63d5
Jacob Riggs <jacobariggs1@gmail.com>
2019-10-20 16:49:17 -0400

    Working game

--- eaui/src/components/Game.vue
+++ eaui/src/components/Game.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="row game-box">
     <div class="column room-box">
-      <h2>{{game.room.name}}</h2>
-      <p>{{game.room.description}}</p>
+      <div class="room-details">
+        <span v-if="exitMessage">{{ exitMessage }}</span>
+        <h2 v-if="enterMessage.length != 0">{{ enterMessage[0].name }}</h2>
+        <span v-if="enterMessage.length != 0">{{ enterMessage[0].description }}</span>
+      </div>
       <ul class="option-list">
-        <li v-for="option in game.options" v-bind:key="option.id">
-          {{option}}
+        <li v-for="(option, key) in options" v-bind:key="option.id">
+          <button class="player-option text-only" v-on:click="exitRoom(key)">{{ option }}</button>
         </li>
       </ul>
-      <button @click="cycleRoom">New Room</button>
+      <button v-if="options.length == 0" @click="cycleRoom">Continue</button>
     </div>
     <div class="column inventory-box">
       <h2>Health</h2>
-      <span>{{game.health}}</span>
+      <span>{{ game.health }}</span>
       <br>
       <h2>Gold</h2>
+      <span>{{ game.gold }}</span>
       <br>
-      <h2 v-if="game.inventory > 0">Inventory</h2>
-      <ul class="inventory" v-if="game.inventory > 0">
-        <li class="row item" v-for="item in items" v-bind:key="item.id">
-          <img>
-          <span :title="item.description">{{item.name}}</span>
-        </li>
+      <h2 v-if="game.inventory.length > 0">Inventory</h2>
+      <ul class="inventory" v-if="game.inventory.length > 0">
+        <p class="row item" v-for="item in game.inventory" :key="item">{{ item }}</p>
       </ul>
     </div>
   </div>
@@ -36,13 +37,17 @@ import url from '../url'
 
 export default {
   name: "game",
-  data: {
-    game: null, //ADD game
-  },
-  mounted: function() {
-    if(this.saved) {
-    } else {
+  data() {
+    return {
+      game: {},
+      options: [],
+      enterMessage: "",
+      exitMessage: "",
     }
+  },
+  beforeMount: function() {
+    this.game = this.getGame();
+    this.game.username = this.getUsername();
     this.enterRoom();
   },
   methods: Object.assign (
@@ -60,7 +65,7 @@ export default {
               'Content-Type': 'application/json',
             },
             data: {
-              game: this.game
+              game: this.game,
             },
           }
           axios(options).then(response =>{
@@ -72,7 +77,8 @@ export default {
                 type: 'is-danger'
               })
             } else {
-              this.game = response.data.game;
+              this.exitMessage = "";
+              this.enterMessage = response.data.result;
               this.options = response.data.options;
             }
           })
@@ -85,28 +91,110 @@ export default {
           })
         }
       },
-      exitRoom(optionID) {
-        
+      exitRoom(option) {
+        try {
+          let requestURL = url + '/exit';
+
+          var axios = require('axios');
+
+          const options = {
+            url: requestURL,
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            data: {
+              game: this.game,
+              optionID: option,
+            },
+          }
+          axios(options).then(response =>{
+            if(response.data === "Failure"){
+              this.$toast.open({
+                duration: 3000,
+                message: 'The abyss seems to be missing a room.',
+                position: 'is-bottom',
+                type: 'is-danger'
+              })
+            } else {
+              if(response.data.status == "Win") {
+                this.endGame("Win");
+              } else if (response.data.status == "Lose") {
+                this.endGame("Lose");
+              } else {
+                this.game = response.data.game;
+                this.exitMessage = response.data.result;
+                this.enterMessage = "";
+                this.options = [];
+              }
+            }
+          })
+        } catch (e) {
+          this.$toast.open({
+            duration: 3000,
+            message: 'Something is wrong in the abyss. Come back later.',
+            position: 'is-bottom',
+            type: 'is-danger'
+          })
+        }
       },
       cycleRoom() {
-        this.potentialRooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+        let potentialRooms = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
         //Remove current and recent rooms from potential rooms
-        for(recRoom in recentRooms) {
-          for(let i = 0; i < this.potentialRooms.length; i++) {
-            if(this.potentialRooms[i] == recRoom || this.potentialRooms[i] == this.game.roomID) {
-              this.potentialRooms.splice(i, 1);
+        for(var index in this.game.recentRooms) {
+          for(let i = 0; i < potentialRooms.length; i++) {
+            if(potentialRooms[i] == this.game.recentRooms[index] || potentialRooms[i] == this.game.roomID) {
+              potentialRooms.splice(i, 1);
               i--;
             }
           }
         }
         var temp = this.game.roomID;
-        var random = Math.floor(Math.random() * this.potentialRooms.length);
-        this.game.roomID = this.potentialRooms[random];
-        this.potentialRooms[random] = this.game.recentRooms.shift();
+        var random = Math.floor(Math.random() * potentialRooms.length);
+        this.game.roomID = potentialRooms[random];
+        potentialRooms[random] = this.game.recentRooms.shift();
         this.game.recentRooms.push(temp);
+
+        this.enterRoom();
+      },
+      endGame(result) {
+        try {
+          let requestURL = url + '/endgame';
+
+          var axios = require('axios');
+
+          const options = {
+            url: requestURL,
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            data: {
+              game: this.game,
+              status: result,
+            },
+          }
+          axios(options).then(response =>{
+            if(response.data === "Failure"){
+              this.$toast.open({
+                duration: 3000,
+                message: 'An error has occurred.',
+                position: 'is-bottom',
+                type: 'is-danger'
+              })
+            }
+          })
+        } catch (e) {
+          this.$toast.open({
+            duration: 3000,
+            message: 'Something is wrong in the abyss. Come back later.',
+            position: 'is-bottom',
+            type: 'is-danger'
+          })
+        }
       }
     },
-    mapGetters(['game'])
+    mapGetters(['getGame', 'getUsername'])
   )
 };
 
@@ -114,6 +202,18 @@ export default {
 
 
 <style scoped>
+
+.player-option:hover {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+
+
+.player-option {
+  background:none;
+  border:none;
+  font-size: 150%;
+}
 
 .game-box {
   height: 86vh;
@@ -140,7 +240,7 @@ export default {
 
 h2 {
   font-weight: bold;
-  font-size: large;
+  font-size: 200%;
 }
 
 .inventory {
